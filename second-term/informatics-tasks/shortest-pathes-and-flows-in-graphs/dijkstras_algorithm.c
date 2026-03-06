0661dc16b1f56f33ff2998593371a8443be54cf6
Spatium-Lustrator <tamaradmitrieva777@yandex.ru>
2026-03-06 20:44:55 +0300

    Trying to fix dijkstras algo...

--- second-term/informatics-tasks/shortest-pathes-and-flows-in-graphs/dijkstras_algorithm.c
+++ second-term/informatics-tasks/shortest-pathes-and-flows-in-graphs/dijkstras_algorithm.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define INACCURACY 0.00000000000000000000000001
+#define INACCURACY 0.0000000000000000000001
 
 void update_neighbors_costs(double **adjacency_matrix, double *distances, int current_vertex, int count_of_vertexes) {
     int l;
@@ -11,11 +11,25 @@ void update_neighbors_costs(double **adjacency_matrix, double *distances, int cu
     for (l=0; l<count_of_vertexes; l++) {
         new_cost = current_cost + adjacency_matrix[current_vertex][l];
 
-        if (!(adjacency_matrix[current_vertex][l] < INACCURACY) && new_cost < distances[l]) {
+        if (!(adjacency_matrix[current_vertex][l]< INACCURACY) && new_cost < distances[l]) {
             distances[l] = new_cost;
         }
     }
 
+}
+
+int find_node_with_lowest_cost(double *distances, int *processed, int count_of_vertexes) {
+    int result=-1, i;
+    double current_min_distance = INFINITY;
+
+    for (i=0; i<count_of_vertexes; i++) {
+        if (current_min_distance > distances[i] && !processed[i]) {
+            result = i;
+            current_min_distance = distances[i];
+        }
+    }
+
+    return result;
 }
 
 double find_shortest_path_weight(double **adjacency_matrix, int from_vertex, int to_vertex, int count_of_vertexes) {
@@ -45,27 +59,27 @@ double find_shortest_path_weight(double **adjacency_matrix, int from_vertex, int
 
 int main(void) {
     int i, j;
-    int count_of_vertexes, index_from_vertex, index_to_vertex;
-    int **adjacency_matrix;
+    int count_of_vertexes, from_vertex, to_vertex;
+    double** adjacency_matrix; 
 
-    scanf("%d %d %d", &count_of_vertexes, &index_from_vertex, &index_to_vertex);
-
-    adjacency_matrix = (int**) malloc(sizeof(int*)*count_of_vertexes);
+    scanf("%d %d %d", &count_of_vertexes, &from_vertex, &to_vertex);
+    adjacency_matrix = (double**) malloc(sizeof(double*)*count_of_vertexes);
 
     for (i=0; i<count_of_vertexes; i++) {
-        adjacency_matrix[i] = (int*) malloc(sizeof(int)*count_of_vertexes);
-        for (j=0; j<count_of_vertexes; j++) {
-            scanf("%d", adjacency_matrix[i] + j);
+        adjacency_matrix[i] = (double*) malloc(sizeof(double)*count_of_vertexes);
+        for (j=0; j<count_of_vertexes; j++)  {
+            scanf("%lf", &adjacency_matrix[i][j]);
         }
     }
 
-    for (i=0; i<count_of_vertexes; i++) {
-        free(adjacency_matrix[i]);
-    }
+    printf("%lf", find_shortest_path_weight(adjacency_matrix, from_vertex, to_vertex, count_of_vertexes));
 
-    printf("%lf", find_shortest_path_weight(adjacency_matrix, index_from_vertex, index_to_vertex, count_of_vertexes));
-
+    for (i=0; i<count_of_vertexes; i++) free(adjacency_matrix[i]);
     free(adjacency_matrix);
 
+
+
     return 0;
+
+
 }
