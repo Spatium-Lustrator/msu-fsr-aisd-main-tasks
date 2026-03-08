f48a527a32597083407a6567a7d1bd04a23bfa22
Spatium-Lustrator <tamaradmitrieva777@yandex.ru>
2026-03-08 14:20:00 +0300

    New variation of the third task from 2 semester block

---
+++ second-term/informatics-tasks/shortest-pathes-and-flows-in-graphs/the_left_maze_splitted_directions.c
@@ -0,0 +1,246 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define INFINITY 2147483647
+#define WEST 0
+#define EAST 1
+#define NORTH 2
+#define SOUTH 3
+
+typedef struct distance_and_remaining_turns {
+    int distance;
+    int direction;
+    int remaining_turns;
+    int index;
+
+} distance_and_remaining_turns;
+
+typedef struct queue_element {
+    distance_and_remaining_turns *cell_and_direction;
+    struct queue_element *next;
+
+} queue_element;
+
+typedef struct queue {
+    queue_element *head;
+    queue_element *tail;
+    int size;
+
+} queue;
+
+void push(queue *queue, distance_and_remaining_turns *cell) {
+    queue_element *new_element = (queue_element*) malloc(sizeof(queue_element));
+
+    new_element->next = NULL;
+    new_element->cell_and_direction = cell;
+
+    if (queue->size > 0) {
+        queue->tail->next = new_element;
+        queue->tail = new_element;
+    } else {
+        queue->head = queue->tail = new_element;
+    }
+
+    queue->size += 1;
+}
+
+distance_and_remaining_turns* pop(queue *queue) {
+    distance_and_remaining_turns *result = NULL;
+    queue_element *head;
+
+    if (queue->size > 0) {
+        head = queue->head;
+
+        result = head->cell_and_direction;
+        if (queue->size == 1) queue->tail = NULL;
+        queue->head = head->next;
+        queue->size -= 1;
+        
+        free(head);
+
+    }
+
+    return result;
+}
+
+int is_not_empty(queue *queue) {
+    return (queue->size > 0);
+}
+
+void update_distance_info_and_push(queue *bfs_queue, distance_and_remaining_turns *cell_to_update, distance_and_remaining_turns *cell_from_update, int turn_consumed) {
+    cell_to_update->distance = cell_from_update->distance + 1;
+    cell_to_update->remaining_turns = cell_from_update->remaining_turns - turn_consumed;
+    push(bfs_queue, cell_to_update);
+}
+
+
+void update_neighbors_info(int *hall_scheme, distance_and_remaining_turns ***distances, queue *bfs_queue, distance_and_remaining_turns *current_cell, int hall_width, int hall_length) {
+    int cell_to_the_west_index, cell_to_the_east_index, cell_to_the_north_index, cell_to_the_south_index;
+    int current_cell_index = current_cell->index;
+    int total_cell_count = hall_length*hall_width;
+    int got_from = current_cell->direction;
+
+
+    if (current_cell_index % hall_length == 0) {
+        cell_to_the_west_index = -1;
+    } else {
+        cell_to_the_west_index= current_cell_index-1;
+    }
+
+
+    if ((current_cell_index+1) % hall_length == 0) {
+        cell_to_the_east_index = -1;
+    } else {
+        cell_to_the_east_index = current_cell_index+1;
+
+    }
+
+    cell_to_the_north_index = current_cell_index-hall_length;
+    cell_to_the_south_index = current_cell_index+hall_length;
+
+    if (got_from != EAST && 0 <= cell_to_the_west_index  && cell_to_the_west_index < total_cell_count && hall_scheme[cell_to_the_west_index] != 1 && (distances[WEST][cell_to_the_west_index]->distance == INFINITY)) {
+        if (current_cell->direction == SOUTH && current_cell->remaining_turns >= 1) {
+            update_distance_info_and_push(bfs_queue, distances[WEST][cell_to_the_west_index], distances[current_cell->direction][current_cell_index], 1);
+        } else if (current_cell->direction != SOUTH) {
+            update_distance_info_and_push(bfs_queue, distances[WEST][cell_to_the_west_index], distances[current_cell->direction][current_cell_index], 0);
+        }
+    }
+
+    if (got_from != WEST && 0 <= cell_to_the_east_index  && cell_to_the_east_index < total_cell_count && hall_scheme[cell_to_the_east_index] != 1 && (distances[EAST][cell_to_the_east_index]->distance == INFINITY)) {
+        if (current_cell->direction == NORTH && current_cell->remaining_turns >= 1) {
+            update_distance_info_and_push(bfs_queue, distances[EAST][cell_to_the_east_index], distances[current_cell->direction][current_cell_index], 1);
+        } else if (current_cell->direction != NORTH) {
+            update_distance_info_and_push(bfs_queue, distances[EAST][cell_to_the_east_index], distances[current_cell->direction][current_cell_index], 0);
+        }
+    }
+
+    if (got_from != SOUTH && 0 <= cell_to_the_north_index  && cell_to_the_north_index < total_cell_count && hall_scheme[cell_to_the_north_index] != 1 && (distances[NORTH][cell_to_the_north_index]->distance == INFINITY)) {
+        if (current_cell->direction == WEST && current_cell->remaining_turns >= 1) {
+            update_distance_info_and_push(bfs_queue, distances[NORTH][cell_to_the_north_index], distances[current_cell->direction][current_cell_index], 1);
+        } else if (current_cell->direction != WEST) {
+            update_distance_info_and_push(bfs_queue, distances[NORTH][cell_to_the_north_index], distances[current_cell->direction][current_cell_index], 0);
+        }
+    }
+
+    if (got_from != NORTH && 0 <= cell_to_the_south_index  && cell_to_the_south_index < total_cell_count && hall_scheme[cell_to_the_south_index] != 1 && (distances[SOUTH][cell_to_the_south_index]->distance == INFINITY)) {
+        if (current_cell->direction == EAST && current_cell->remaining_turns >= 1) {
+            update_distance_info_and_push(bfs_queue, distances[SOUTH][cell_to_the_south_index], distances[current_cell->direction][current_cell_index], 1);
+        } else if (current_cell->direction != EAST) {
+            update_distance_info_and_push(bfs_queue, distances[SOUTH][cell_to_the_south_index], distances[current_cell->direction][current_cell_index], 0);
+        }
+    }
+
+
+}
+
+void breadth_first_search(int *hall_scheme, distance_and_remaining_turns ***distances, queue *bfs_queue, int start_available_right_turns, int start_cell, int hall_width, int hall_length) {
+    distance_and_remaining_turns *current_cell;
+    int k;
+
+    push(bfs_queue, distances[WEST][start_cell]);
+    push(bfs_queue, distances[EAST][start_cell]);
+    push(bfs_queue, distances[NORTH][start_cell]);
+    push(bfs_queue, distances[SOUTH][start_cell]);
+
+    while (is_not_empty(bfs_queue)) {
+        current_cell = pop(bfs_queue);
+
+        update_neighbors_info(hall_scheme, distances, bfs_queue, current_cell, hall_width, hall_length);
+    }
+
+
+
+}
+
+int main(void) {
+
+    distance_and_remaining_turns ***distances;
+
+    queue *bfs_queue;
+
+    int min_dist_to_target = INFINITY;
+    int *hall_scheme;
+    int start_cell, finish_cell;
+    int number_of_available_right_turns, hall_width, hall_length; 
+    int i, j;
+
+    scanf("%d %d %d", &number_of_available_right_turns, &hall_width, &hall_length);
+
+    distances = (distance_and_remaining_turns***) calloc(4, sizeof(distance_and_remaining_turns**));
+    hall_scheme = (int*) calloc(hall_width*hall_length, sizeof(int));
+
+    bfs_queue = (queue*) calloc(1, sizeof(queue));
+    bfs_queue->size = 0;
+    bfs_queue->head = NULL;
+    bfs_queue->tail = NULL;
+
+    for (i=0; i<4; i++) {
+        distances[i] = (distance_and_remaining_turns**) calloc(hall_length*hall_width, sizeof(distance_and_remaining_turns*));
+        for (j=0; j<hall_length*hall_width; j++) {
+            distances[i][j] = (distance_and_remaining_turns*) calloc(1, sizeof(distance_and_remaining_turns));
+            distances[i][j]->distance = INFINITY;
+            distances[i][j]->direction = i;
+            distances[i][j]->remaining_turns = 0;
+            distances[i][j]->index = j;
+        }
+    }
+
+    for (i=0; i<hall_width*hall_length; i++) {
+        scanf("%d", hall_scheme + i);
+
+        if (hall_scheme[i] == 2) {
+            start_cell = i;
+        } else if (hall_scheme[i] == 3) {
+            finish_cell = i;
+        }
+    }
+
+    for (i=0; i<4; i++) {
+        distances[i][start_cell]->distance = 0;
+        distances[i][start_cell]->remaining_turns = number_of_available_right_turns;
+    }
+
+    breadth_first_search(hall_scheme, distances, bfs_queue, number_of_available_right_turns, start_cell, hall_width, hall_length);
+
+    for (i=0; i<4; i++) {
+        for (j=0; j<hall_length*hall_width; j++) {
+            printf("([%d][%d] ", i, j);
+            if (distances[i][j]->distance == INFINITY) {
+                printf("inf) ");
+            } else {
+                printf("%d) ", distances[i][j]->distance);
+            }
+        }
+        printf("\n");
+    }
+
+
+    for (i=0; i<4; i++) {
+        printf("%d ", distances[i][finish_cell]->distance);
+        if (min_dist_to_target > distances[i][finish_cell]->distance) {
+            min_dist_to_target = distances[i][finish_cell]->distance;
+        }
+
+        for (j=0; j<hall_length*hall_width; j++) {
+            free(distances[i][j]);
+        }
+        free(distances[i]);
+    }
+    printf("\n");
+
+    if (min_dist_to_target == INFINITY) {
+        printf("-1");
+    } else {
+        printf("%d", min_dist_to_target);
+    }
+    
+    free(distances);
+    free(bfs_queue);
+    free(hall_scheme);
+
+
+
+
+    return 0;
+}
